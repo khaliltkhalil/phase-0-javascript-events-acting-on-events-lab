f8707a9b919188277df8de0a4df54c04bf7c4e7b
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-04-10 21:02:23 -0500

    writes index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4';
+
+function moveDodgerLeft() {
+    const currPosition = parseInt(dodger.style.left, 10);
+    if(currPosition > 0) {
+        dodger.style.left = `${currPosition - 1}px`;
+    }
+    
+}
+
+
+function moveDodgerRight() {
+    const currPosition = parseInt(dodger.style.left, 10);
+    if(currPosition < 360) {
+        dodger.style.left = `${currPosition + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', (e) => {    
+    if(e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+
+    if(e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
